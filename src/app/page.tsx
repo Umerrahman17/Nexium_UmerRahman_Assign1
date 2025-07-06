6273b445ef6aa92eee835c379851eabaf600b0fd
Umerrahman17 <umerrahman1711@gmail.com>
2025-07-07 03:48:36 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
-import { Input } from "../components/ui/input";
+//import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 
